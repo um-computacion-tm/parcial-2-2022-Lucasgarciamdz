d645d66e716bef0698f4d46737d6f6b9a90d3375
Lucas García <lu.garcia@alumno.um.edu.ar>
2022-10-26 09:31:56 -0300

    Entrega final

--- compress.py
+++ compress.py
@@ -1,16 +1,15 @@
-
-
 class Compress:
+
     def __init__(self):
         self.compressed = []
         self.values = {}
         self.text = ""
 
-    def compress(self,text):
+    def compress(self, text):
         palabras = text.split(" ")
         palabras_repetidas = []
         x = 1
-        for i in range(len(palabras)): 
+        for i in range(len(palabras)):
             if palabras[i] in palabras_repetidas:
                 self.compressed.append(self.values[palabras[i]])
             else:
@@ -23,5 +22,6 @@ class Compress:
 
     def uncompress(self, compressed, values):
         for i in range(len(compressed)):
-            self.text += list(values.keys())[list(values.values()).index(compressed[i])] + " "
+            self.text += list(values.keys())[list(values.values()).index(
+                compressed[i])] + " "
         return self.text[:-1]
